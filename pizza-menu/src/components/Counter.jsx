1f836980084338d01727ba37ed46627f6c793133
fissid <fissid.ss@gmail.com>
2023-11-10 00:50:16 +0300

    changes

--- pizza-menu/src/components/Counter.jsx
+++ pizza-menu/src/components/Counter.jsx
@@ -3,10 +3,22 @@ export default function Counter() {
   const [myDate, setMyDate] = useState(new Date());
   console.log(myDate);
   return (
-    <>
-      <div className="step"></div>
-      <div className="count"></div>
+    <div className="counter">
+      <div className="step">
+        <button>-</button>
+        <p>
+          Step: <span>1</span>
+        </p>
+        <button>+</button>
+      </div>
+      <div className="step count">
+        <button>-</button>
+        <p>
+          Count: <span>1</span>
+        </p>
+        <button>+</button>
+      </div>
       <p>Today is {}</p>
-    </>
+    </div>
   );
 }
